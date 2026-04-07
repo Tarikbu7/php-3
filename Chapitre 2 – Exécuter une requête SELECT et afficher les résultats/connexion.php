799ced50790b6255d4571fc5d82779bb30b2a653
tarik <bufardiitarik@gmail.com>
2026-04-07 10:00:41 +0100

    Add files via upload

---
+++ Chapitre 2 – Exécuter une requête SELECT et afficher les résultats/connexion.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'goku';
+$username = 'root';
+$password = 'Tarik7gamer';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base $dbname";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
